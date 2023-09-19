637c3ce828d6dbbf70afaaec8da65402d627ea08
mohzaidkhan <khanhamza2014@gmail.com>
2023-09-19 07:18:09 +0530

    Answer 1

---
+++ src/main/java/org/example/SumOfN.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SumOfN {
+    public static int sum(int n){
+        if (n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
